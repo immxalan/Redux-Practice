1607be466e762110326e087437df3f3f7f4e65a3
Allen Do <ad@Allens-MacBook-Pro.local>
2020-02-20 16:22:47 -0800

    Added reducer and actions

---
+++ project/src/reducers/PlayerReducer.js
@@ -0,0 +1,15 @@
+//setup initialState
+const initialState = {
+    players: [],
+    isFetching: false,
+    errors: ""
+}
+
+//setup an empty reducer
+export const playerReducer = (state=initialState, action) => {
+switch(action.type){
+    //setup swith, cases, and default
+    default:
+        return state
+}
+}
